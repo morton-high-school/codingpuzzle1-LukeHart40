3f26792494f98bf183f7fbe9e395e5ac60a505d2
LukeHart40 <lucas.hart@mcusd709.org>
2024-10-17 20:50:02 -0500

    Final Changes
    I think I got to the end.

--- decipher.java
+++ decipher.java
@@ -7,14 +7,28 @@ public class decipher{
         Scanner reader = new Scanner(puzzle);
         String text = reader.nextLine();
 
-        String currentChar = "";
-        String nextChar = "";
+        String firstChar = "";
+        String lastChar = "";
         String longString = "";
-        int vowelCount = 0;
+        int length = 0;
+        String newString = "";
 
         String end = "";
         for(int i=0;i<text.length();i++){
-            System.out.println(text.indexOf("kinnikinnik"));
+            newString = text.substring(i,i+11);
+            for(int j=0;j<newString.length();j++){
+                firstChar = newString.substring(j, j+1);
+                lastChar = newString.substring(10-j, 11-j);
+                if(firstChar.equals(lastChar)){
+                    length++;
+                }else{
+                    length = 0;
+                }
+                if(length==11){
+                    System.out.println(text.substring(i));
+                }
+            }
+            
         }
     } 
 }
@@ -22,4 +36,5 @@ public class decipher{
 //armadillo your next task and keyword are located in the one hundred and five characters after the longest string of the same consecutive character
 //penguin your next task and keyword are located in the eighty characters after the string of length thirty with the most vowels
 //ostrich your final keyword is located in the six characters after a palindrome of length eleven
-//Key words: armadillo/penguin/ostrich
+//yo banana boy badgers
+//Key words: armadillo/penguin/ostrich/badger
